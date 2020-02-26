7356e50a535b79468ccd86856f4fb108ad701fda
Hanna Szczyriec <sisyrinchium02@gmail.com>
2020-02-26 20:28:34 +0300

    add some changes in AddACommentForMoodTest

--- src/test/java/tests/AddACommentForMoodTest.java
+++ src/test/java/tests/AddACommentForMoodTest.java
@@ -6,7 +6,7 @@ import tests.base.BaseTest;
 public class AddACommentForMoodTest extends BaseTest {
     @Test
     public void addAComment(){
-        String comment = "xiyybhmacebgikgltswo";
+        String comment = "kqxjmphjmjzqwqisspfx";
         String message = "8888";
         moodSteps.goToMyUpdates()
                 .addAComment(comment, message);
